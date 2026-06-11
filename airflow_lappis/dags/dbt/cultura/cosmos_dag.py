4cc16e1d8706c88f5bdb32917cc47e2ac43fb737
Caio Melo Borges <caioborges250802@gmail.com>
2026-06-11 08:00:14 -0300

    fix(cosmos): adiciona fallback nas env_vars do profiles.yml para corrigir parsing da DAG

--- airflow_lappis/dags/dbt/cultura/cosmos_dag.py
+++ airflow_lappis/dags/dbt/cultura/cosmos_dag.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-
+from cosmos.profiles import PostgresUserPasswordProfileMapping 
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.constants import DBT_LOG_PATH_ENVVAR
 
@@ -9,9 +9,12 @@ os.makedirs(dbt_log_path, exist_ok=True)
 os.environ[DBT_LOG_PATH_ENVVAR] = dbt_log_path
 
 profile_config = ProfileConfig(
-    profiles_yml_filepath=f"{os.environ['AIRFLOW_REPO_BASE']}/dags/dbt/cultura/profiles.yml",
     profile_name="cultura",
     target_name="prod",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="postgres_default", 
+        profile_args={"schema": "cultura"},
+    )
 )
 
 my_cosmos_dag = DbtDag(
